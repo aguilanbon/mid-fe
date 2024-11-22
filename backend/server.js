ced4a00c4d5857242f53f19b694064db802e5fd6
Bon Aguilan <aguilanbon@gmail.com>
2024-11-22 12:00:43 +0800

    added pg package, postgress db.js config, init getter

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,22 @@
 import express from "express";
 import dotenv from "dotenv";
+import pool from "./config/db.js";
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(express.json());
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
+});
+
+app.get("/api/people", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT * FROM people");
+    res.json(result.rows);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 });

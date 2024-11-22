46575d10cad55349361a5cdb27f82335f7d9047b
Bon Aguilan <aguilanbon@gmail.com>
2024-11-22 19:01:31 +0800

    responsive taskcards, test home cards mock items

--- frontend/src/components/TaskCard.jsx
+++ frontend/src/components/TaskCard.jsx
@@ -1,6 +1,6 @@
 export default function TaskCard({ title, description }) {
   return (
-    <div className="w-96 h-32 bg-white shadow-lg hover:shadow-xl p-4 rounded-md cursor-pointer">
+    <div className="w-full max-w-sm md:max-w-lg bg-white shadow-lg hover:shadow-xl p-4 rounded-md cursor-pointer">
       <h3 className="text-lg font-bold">{title}</h3>
       <p className="overflow-hidden line-clamp-2">{description}</p>
     </div>
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,49 @@
-import Button from "../components/Button";
+import { Link } from "react-router-dom";
+import TaskCard from "../components/TaskCard";
 
 export default function Home() {
   return (
-    <div className="">
-      <h1>Tasker</h1>
-      <Button>Create Task</Button>
+    <div className="w-full flex items-center justify-center">
+      <div className="w-full max-w-5xl space-y-4 py-8 px-10">
+        <div className="w-full flex justify-end">
+          <Link
+            to="/create"
+            className="px-4 py-2 text-sm font-medium text-white bg-blue-600 rounded-md hover:bg-blue-700"
+          >
+            Create Task
+          </Link>
+        </div>
+        <div className="grid grid-cols-1 place-items-center items-center md:grid-cols-2  gap-6">
+          <TaskCard
+            title={`Title`}
+            description={`  Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore
+            totam inventore ut iure possimus, quam distinctio sint corrupti vel
+            corporis alias recusandae odio? Quam harum doloribus laborum
+            architecto dolores quibusdam?`}
+          />
+          <TaskCard
+            title={`Title`}
+            description={`  Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore
+            totam inventore ut iure possimus, quam distinctio sint corrupti vel
+            corporis alias recusandae odio? Quam harum doloribus laborum
+            architecto dolores quibusdam?`}
+          />
+          <TaskCard
+            title={`Title`}
+            description={`  Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore
+            totam inventore ut iure possimus, quam distinctio sint corrupti vel
+            corporis alias recusandae odio? Quam harum doloribus laborum
+            architecto dolores quibusdam?`}
+          />
+          <TaskCard
+            title={`Title`}
+            description={`  Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore
+            totam inventore ut iure possimus, quam distinctio sint corrupti vel
+            corporis alias recusandae odio? Quam harum doloribus laborum
+            architecto dolores quibusdam?`}
+          />
+        </div>
+      </div>
     </div>
   );
 }

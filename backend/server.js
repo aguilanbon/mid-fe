3f848a0509f15c62a015935ea73e6525ce645cbf
Bon Aguilan <aguilanbon@gmail.com>
2024-11-22 19:36:55 +0800

    added allowed origin cors option

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,18 @@ import express from "express";
 import dotenv from "dotenv";
 import tasksRouter from "./routes/tasks.route.js";
 import { initDatabase } from "./config/db.js";
+import cors from "cors";
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+const corsOptions = {
+  origin: "http://localhost:5173",
+};
+
 app.use(express.json());
+app.use(cors(corsOptions));
 
 app.use("/api/tasks", tasksRouter);
 

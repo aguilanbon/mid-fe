bd2bd1f65310f87b3f1e4a6558ab84a8957217e2
Bon Aguilan <aguilanbon@gmail.com>
2024-11-22 10:43:21 +0800

    init files

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1 className="text-3xl font-bold underline text-teal-500">
+          Hello World
+        </h1>
+      </div>
+    </>
+  );
+}
+
+export default App;

2198811dd7124823797542e4cf78c662b605f6ac
Bon Aguilan <aguilanbon@gmail.com>
2024-11-22 13:11:00 +0800

    update person route, controller

--- backend/controller/people.controller.js
+++ backend/controller/people.controller.js
@@ -18,7 +18,28 @@ export const createPeople = async (req, res) => {
     );
     res
       .status(201)
-      .json({ message: "People created successfully", data: result.rows[0] });
+      .json({ message: "Person created successfully", data: result.rows[0] });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+export const updatePeople = async (req, res) => {
+  const { id } = req.params;
+  const { first_name, last_name, address } = req.body;
+  try {
+    const result = await pool.query(
+      "UPDATE people SET first_name = $1, last_name = $2, address = $3 WHERE id = $4 RETURNING *",
+      [first_name, last_name, address, id]
+    );
+
+    if (result.rows.length === 0) {
+      return res.status(404).json({ message: "Person not found" });
+    }
+
+    res
+      .status(200)
+      .json({ message: "Person updated successfully", data: result.rows[0] });
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
--- backend/routes/people.route.js
+++ backend/routes/people.route.js
@@ -1,8 +1,13 @@
 import express from "express";
-import { fetchPeople, createPeople } from "../controller/people.controller.js";
+import {
+  fetchPeople,
+  createPeople,
+  updatePeople,
+} from "../controller/people.controller.js";
 const peopleRouter = express.Router();
 
 peopleRouter.get("/", fetchPeople);
 peopleRouter.post("/", createPeople);
+peopleRouter.put("/:id", updatePeople);
 
 export default peopleRouter;
